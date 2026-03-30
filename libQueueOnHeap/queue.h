ca3ca16b5cdc4e840e2c26a0b49ff7090aff1d78
midi <fragemorenno@gmail.com>
2026-03-30 10:15:55 +0300

    added QueueOnHeap and func: print k elements from queue

---
+++ libQueueOnHeap/queue.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../libHeap/heap.h"
+template<typename Pr, typename Val>
+class QueueOnHeap {
+private:
+	MinHeap<std::pair<Pr, Val>> heap;
+public:
+	QueueOnHeap() = default;
+	std::pair<Pr, Val> pop() {
+		if (heap.empty()) {
+			throw std::out_of_range("Heap is empty");
+		}
+		return heap.pop();
+
+	}
+	void push(const Pr& pr, const Val& val) {
+		std::pair<Pr, Val> pairr = std::make_pair(pr, val);
+		heap.add(pairr);
+	}
+	bool empty() const {
+		return heap.empty();
+	}
+	size_t size() const {
+		return heap.size();
+	}
+	 
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,19 @@
 #include "Tree.h"
 #include "../algorithms/algorithms.h"
 #include "../libHeap/heap.h"
+#include "../libQueueOnHeap/queue.h"
+template<typename Pr, typename Val>
+void printK(const std::vector<std::pair<Pr, Val>> data, int k) {
+	QueueOnHeap<Pr, Val> pq;
+	for (const auto& item : data) {
+		pq.push(item.first, item.second);
+	}
+	for (int i = 0; i < k && !pq.empty(); i++) {
+		auto item = pq.pop();
+		std::cout << "\n" <<"Приоритет: " <<item.first << " " << item.second;
+	}
+}
+
 
 void main() {
 	SetConsoleCP(65001);      
@@ -20,6 +33,32 @@ void main() {
 	for (int i = 0; i < 20; i++) {
 		std::cout << " " << v[i];
 	}
+
+	QueueOnHeap<int, std::string> _queue;
+	std::vector < std::pair<int, std::string>> date;
+	for (int i = 0; i < 20; i++) {
+		std::string val = "data_" + std::to_string(i);
+		int priority = rand() % 100;
+		date.push_back({ priority, val });
+	}
+	printK(date, 15);
+
+	//while (true) {
+	//	std::cout << "\nприоритет:\n";
+	//	int a;
+	//	std::cin >> a;
+	//	if (a < 0) {
+	//		break;
+	//	}
+	//	std::cout << "\nстрока:\n";
+	//	std::string str;
+	//	std::cin >> str;
+	//	_queue.push(a, str);
+
+	//}
+	//while (!_queue.empty()) {
+	//	std::cout << "\n" << _queue.pop();
+	//}
 	
 		
 	
