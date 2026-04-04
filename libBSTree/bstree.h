e37de87be9cdb4f5422d253bc316591e0e77bb2f
midi <fragemorenno@gmail.com>
2026-04-05 00:13:06 +0300

    added BSTree and 3 methods: find, insert, remove

--- libBSTree/bstree.h
+++ libBSTree/bstree.h
@@ -1,1 +1,117 @@
 #pragma once
+#include <iostream>
+#include <utility>
+
+template<typename Tkey, typename Tvalue>
+struct BSTNode
+{
+    std::pair<Tkey, Tvalue> data;
+    BSTNode<Tkey, Tvalue>* left;
+    BSTNode<Tkey, Tvalue>* right;
+};
+
+template<typename Tkey, typename Tvalue>
+class BSTree {
+private:
+    BSTNode<Tkey, Tvalue>* _root;
+    BSTNode<Tkey, Tvalue>* FindNode(const Tkey& key, BSTNode<Tkey, Tvalue>* pNode) const {
+        if (pNode == nullptr) {
+            return nullptr;
+        }
+        if (key < pNode->data.first) {
+            return FindNode(key, pNode->left);
+        }
+        if (key > pNode->data.first) {
+            return FindNode(key, pNode->right);
+        }
+        return pNode;
+    }
+
+public:
+    BSTree() : _root(nullptr) {}
+
+    bool is_empty() const noexcept {
+        return _root == nullptr;
+    }
+    Tvalue* find(const Tkey& key) const noexcept {
+        BSTNode<Tkey, Tvalue>* pNode = FindNode(key, _root);
+        if (pNode == nullptr) {
+            return nullptr;
+        }
+        return &pNode->data.second;
+    }
+
+    void insert(const Tkey& key, const Tvalue& val) {
+        if (_root == nullptr) {
+            _root = new BSTNode<Tkey, Tvalue>{ { key, val }, nullptr, nullptr };
+            return;
+        }
+
+        BSTNode<Tkey, Tvalue>* pCurrent = _root;
+        BSTNode<Tkey, Tvalue>* pParent = nullptr;
+        while (pCurrent != nullptr) {
+            pParent = pCurrent;
+            if (key < pCurrent->data.first) {
+                pCurrent = pCurrent->left;
+            }
+            else if (key > pCurrent->data.first) {
+                pCurrent = pCurrent->right;
+            }
+            else {
+                return;
+            }
+        }
+        BSTNode<Tkey, Tvalue>* pNew = new BSTNode<Tkey, Tvalue>{ {key, val}, nullptr, nullptr };
+        if (key < pParent->data.first) {
+            pParent->left = pNew;
+        }
+        else {
+            pParent->right = pNew;
+        }
+    }
+    void remove(const Tkey& key) {
+        BSTNode<Tkey, Tvalue>* pCurrent = _root;
+        BSTNode<Tkey, Tvalue>* pParent = nullptr;
+
+        // 1. Поиск удаляемого узла и его родителя
+        while (pCurrent != nullptr && pCurrent->data.first != key) {
+            pParent = pCurrent;
+            if (key < pCurrent->data.first)
+                pCurrent = pCurrent->left;
+            else
+                pCurrent = pCurrent->right;
+        }
+
+        if (pCurrent == nullptr) return;
+
+        // 2. Случай: У узла два потомка
+        if (pCurrent->left != nullptr && pCurrent->right != nullptr) {
+            BSTNode<Tkey, Tvalue>* pSuccessor = pCurrent->right;
+            BSTNode<Tkey, Tvalue>* pSuccessorParent = pCurrent;
+
+            while (pSuccessor->left != nullptr) {
+                pSuccessorParent = pSuccessor;
+                pSuccessor = pSuccessor->left;
+            }
+
+            pCurrent->data = pSuccessor->data;
+            pParent = pSuccessorParent;
+            pCurrent = pSuccessor;
+        }
+
+        // 3. Случай: У узла один потомок или он — лист [4, 5]
+        BSTNode<Tkey, Tvalue>* pChild = (pCurrent->left != nullptr) ? pCurrent->left : pCurrent->right;
+
+        if (pParent == nullptr) {
+            _root = pChild;
+        }
+        else {
+            if (pParent->left == pCurrent)
+                pParent->left = pChild;
+            else
+                pParent->right = pChild;
+        }
+
+        delete pCurrent;
+    }
+};
--- tests/tree.cpp
+++ tests/tree.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include "Tree.h"
-
+#include "../libBSTree/bstree.h"
 // ==============================================================
 // 1. БАЗОВЫЕ ОПЕРАЦИИ (ВСТАВКА И ПУСТОТА)
 // ==============================================================
@@ -85,4 +85,115 @@ TEST(TreeFullTest, TypesAndVisualOutput) {
     tree.lrc();
 
     SUCCEED();
+}
+
+// Тест 1: Проверка начального состояния (пустое дерево)
+TEST(BSTreeTest, NewTreeIsEmpty) {
+    BSTree<int, std::string> tree;
+    EXPECT_TRUE(tree.is_empty());
+}
+
+// Тест 2: Проверка вставки одного элемента
+TEST(BSTreeTest, InsertSingleElement) {
+    BSTree<int, std::string> tree;
+    tree.insert(10, "ten");
+
+    EXPECT_FALSE(tree.is_empty());
+
+    std::string* value = tree.find(10);
+    ASSERT_NE(value, nullptr); // Указатель не должен быть нулевым
+    EXPECT_EQ(*value, "ten");
+}
+
+// Тест 3: Поиск несуществующего ключа
+TEST(BSTreeTest, FindNonExistentKey) {
+    BSTree<int, std::string> tree;
+    tree.insert(10, "ten");
+
+    EXPECT_EQ(tree.find(20), nullptr); // Должен вернуть nullptr
+}
+
+// Тест 4: Вставка нескольких элементов и сохранение свойств поиска
+TEST(BSTreeTest, InsertMultipleElements) {
+    BSTree<int, int> tree;
+    tree.insert(50, 500);
+    tree.insert(30, 300);
+    tree.insert(70, 700);
+    tree.insert(20, 200);
+    tree.insert(40, 400);
+
+    // Проверяем наличие всех ключей
+    EXPECT_EQ(*tree.find(50), 500);
+    EXPECT_EQ(*tree.find(30), 300);
+    EXPECT_EQ(*tree.find(70), 700);
+    EXPECT_EQ(*tree.find(20), 200);
+    EXPECT_EQ(*tree.find(40), 400);
+}
+
+// Тест 5: Проверка на отсутствие дубликатов (согласно логике insert из лекций)
+TEST(BSTreeTest, InsertDuplicateKeyDoesNothing) {
+    BSTree<int, std::string> tree;
+    tree.insert(1, "original");
+    tree.insert(1, "duplicate"); // Вторая вставка не должна сработать
+
+    std::string* value = tree.find(1);
+    ASSERT_NE(value, nullptr);
+    EXPECT_EQ(*value, "original"); // Значение не должно измениться
+}
+// Тест удаления листа [1]
+TEST(BSTreeDeleteTest, DeleteLeaf) {
+    BSTree<int, std::string> tree;
+    tree.insert(10, "root");
+    tree.insert(5, "leaf");
+
+    tree.remove(5);
+    EXPECT_EQ(tree.find(5), nullptr);
+    EXPECT_NE(tree.find(10), nullptr);
+}
+
+// Тест удаления узла с одним потомком [2]
+TEST(BSTreeDeleteTest, DeleteNodeWithOneChild) {
+    BSTree<int, std::string> tree;
+    tree.insert(10, "root");
+    tree.insert(5, "parent");
+    tree.insert(3, "child"); // Узел 5 имеет одного потомка 3
+
+    tree.remove(5);
+    EXPECT_EQ(tree.find(5), nullptr);
+    EXPECT_NE(tree.find(3), nullptr); // 3 должен "подтянуться" на место 5
+}
+
+// Тест удаления узла с двумя потомками [2]
+TEST(BSTreeDeleteTest, DeleteNodeWithTwoChildren) {
+    BSTree<int, std::string> tree;
+    tree.insert(10, "root");
+    tree.insert(5, "node_to_delete");
+    tree.insert(3, "left");
+    tree.insert(7, "right");
+
+    tree.remove(5);
+    EXPECT_EQ(tree.find(5), nullptr);
+    EXPECT_NE(tree.find(3), nullptr);
+    EXPECT_NE(tree.find(7), nullptr);
+}
+
+// Тест удаления корня
+TEST(BSTreeDeleteTest, DeleteRoot) {
+    BSTree<int, std::string> tree;
+    tree.insert(10, "root");
+    tree.insert(15, "right");
+
+    tree.remove(10);
+    EXPECT_EQ(tree.find(10), nullptr);
+    EXPECT_FALSE(tree.is_empty());
+    EXPECT_NE(tree.find(15), nullptr);
+}
+
+// Тест удаления несуществующего ключа
+TEST(BSTreeDeleteTest, DeleteNonExistentKey) {
+    BSTree<int, std::string> tree;
+    tree.insert(10, "root");
+
+    tree.remove(99); // Не должно вызывать ошибок
+    EXPECT_NE(tree.find(10), nullptr);
 }
