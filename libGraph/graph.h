056c1d1d71ad0d0eb3522094fe1b6736f7cbf39a
midi <fragemorenno@gmail.com>
2026-04-19 22:35:36 +0300

    graph done

--- libGraph/graph.h
+++ libGraph/graph.h
@@ -21,14 +21,35 @@ private:
 	bool isWeighted;
 	std::vector<Vertex<T>> vertices; 
 public:
-	AdjListGraph(int n, bool oriented, bool weighted, std::vector<std::pair<T, T>>_data) {
+	AdjListGraph(int n, bool oriented, bool weighted, std::vector<std::pair<T, T>> _data) {
 		count_vertices = n;
 		isOriented = oriented;
 		isWeighted = weighted;
+		vertices.resize(n);
+		for (int i = 0; i < n; i++) {
+			vertices[i].data = (T)i;
+		}
 		for (const auto& p : _data) {
+			int u_ind = (int)p.first;
+			int v_ind = (int)p.second;
+
+			if (u_ind >= count_vertices || v_ind >= count_vertices) continue;
+
+			int w = 1;
+
+			// прямая связь
 			Edge<T> new_edge;
 			new_edge.to = p.second;
-			
+			new_edge.weight = w;
+			vertices[u_ind].neighbors.push_back(new_edge);
+
+			// обратная связь
+			if (!isOriented) {
+				Edge<T> reserve_edge;
+				reserve_edge.to = p.first;
+				reserve_edge.weight = w;
+				vertices[v_ind].neighbors.push_back(reserve_edge);
+			}
 		}
 	}
 	void add_edge(T u, T v, int weight = 1) {
@@ -64,5 +85,31 @@ public:
 			vertices[v_ind].neighbors.remove[u];
 		}
 
+	}
+	void delete_vertex(T v_to_delete) {
+		int del_idx = (int)v_to_delete;
+		if (del_idx >= count_vertices) return;
+
+		vertices.erase(vertices.begin() + del_idx);
+		count_vertices--;
+		for (int i = 0; i < count_vertices; i++) {
+
+			if ((int)vertices[i].data > del_idx) {
+				vertices[i].data = (T)((int)vertices[i].data - 1);
+			}
+			auto it = vertices[i].neighbors.begin();
+			while (it != vertices[i].neighbors.end()) {
+
+				if ((int)it->to == del_idx) {
+					it = vertices[i].neighbors.erase(it);
+				}
+				else {
+					if ((int)it->to > del_idx) {
+						it->to = (T)((int)it->to - 1);
+					}
+					++it;
+				}
+			}
+		}
 	}
 };
